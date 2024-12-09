805491968652bec5bc140debb235257b54b259c4
YJR117 <133361144+EdricYeo117@users.noreply.github.com>
2024-12-09 19:37:06 +0800

    Queues leet code questions

--- LeetCode/LeetCode Queue/numberOfPeopleAwareOfASecret.cpp
+++ LeetCode/LeetCode Queue/numberOfPeopleAwareOfASecret.cpp
@@ -1,56 +1,44 @@
 /*
 On day 1, one person discovers a secret.
 
-You are given an integer delay, which means that each person will share the secret with a new person every day, 
-    starting from delay days after discovering the secret. You are also given an integer forget, 
-    which means that each person will forget the secret forget days after discovering it. 
+You are given an integer delay, which means that each person will share the secret with a new person every day,
+    starting from delay days after discovering the secret. You are also given an integer forget,
+    which means that each person will forget the secret forget days after discovering it.
 A person cannot share the secret on the same day they forgot it, or on any day afterwards.
 
-Given an integer n, return the number of people who know the secret at the end of day n. 
+Given an integer n, return the number of people who know the secret at the end of day n.
 Since the answer may be very large, return it modulo 109 + 7.
 */
 
+#include <iostream>
 #include <queue>
 using namespace std;
 
-class Solution {
-public:
-    int peopleAwareOfSecret(int n, int delay, int forget) {
-        const int mod = 1e9 + 7;
-
-        // Queue to track sharers (startDay, sharers)
-        queue<pair<int, int>> q;
-        int totalSharers = 0;    // Total number of people actively sharing the secret
-        int totalAware = 0;      // Total number of people aware of the secret
-
-        // Day 1: One person knows the secret and starts sharing
-        q.push({1, 1});
-        totalSharers = 1;
-
-        // Simulate each day
-        for (int day = 1; day <= n; day++) {
-            // Remove people who forget the secret today
-            while (!q.empty() && q.front().first + forget == day) {
-                totalSharers = (totalSharers - q.front().second + mod) % mod;
-                q.pop();
-            }
-
-            // Add people who start sharing today
-            if (!q.empty() && q.front().first + delay == day) {
-                q.push({day, totalSharers});
-            }
-
-            // Update the total number of people aware of the secret
-            totalAware = (totalSharers + totalAware) % mod;
+int peopleAwareOfSecret(int n, int delay, int forget) {
+    const int M = 1e9 + 7;
+    int curr = 0;
+    int ans = 1;
+    queue<pair<int,int>> delayQ, forgetQ;
+    delayQ.push({1,1});
+    forgetQ.push({1,1});
+    for (int i = 1; i <= n; i++) {
+        if (!forgetQ.empty() && forgetQ.front().first + forget <= i) {
+            auto front = forgetQ.front();
+            forgetQ.pop();
+            auto no = front.second;
+            ans = (ans - no + M) % M;
+            curr = (curr - no + M) % M;
         }
-
-        // Final count: Sum all people in the queue who haven't forgotten the secret
-        totalAware = 0;
-        while (!q.empty()) {
-            totalAware = (totalAware + q.front().second) % mod;
-            q.pop();
+        if (!delayQ.empty() && delayQ.front().first + delay <= i) {
+            auto front = delayQ.front();
+            delayQ.pop();
+            curr = (curr + front.second) % M;
         }
-
-        return totalAware;
+        if (curr > 0) {
+            ans = (ans + curr) % M;
+            delayQ.push({i, curr});
+            forgetQ.push({i, curr});
+        }
     }
+    return ans;
 };
